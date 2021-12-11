db602d7cd59d9f0212d7ba682b03566cf662bf04
Edgar Žigis <tranced.freak@gmail.com>
2021-12-11 19:40:32 +0200

    MOBILE-448: Fix color customisations for country selection

--- Sources/Kevin/Accounts/CountrySelection/Cells/CountryCell.swift
+++ Sources/Kevin/Accounts/CountrySelection/Cells/CountryCell.swift
@@ -20,6 +20,8 @@ internal class CountryCell : UITableViewCell {
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
+        contentView.backgroundColor = Kevin.shared.theme.primaryBackgroundColor
+        
         backgroundOverlay.isHidden = true
         backgroundOverlay.backgroundColor = Kevin.shared.theme.selectedOnPrimaryColor
         backgroundOverlay.translatesAutoresizingMaskIntoConstraints = false
@@ -45,6 +47,8 @@ internal class CountryCell : UITableViewCell {
         titleLabel.leftAnchor.constraint(equalTo: iconImageView.rightAnchor, constant: 16).isActive = true
         titleLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.5).isActive = true
         titleLabel.heightAnchor.constraint(equalTo: contentView.heightAnchor).isActive = true
+        
+        contentView.backgroundColor = 
     }
     
     required init?(coder aDecoder: NSCoder) {
