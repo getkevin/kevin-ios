73fa9f80795ba3c807c1929178a14e0a74677049
Edgar Žigis <tranced.freak@gmail.com>
2021-12-11 19:57:05 +0200

    MOBILE-449: Add an example on how to modify theme (#17)

---
+++ demo/KevinDemo/Styles/DemoKevinTheme.swift
@@ -0,0 +1,22 @@
+//
+//  DemoKevinTheme.swift
+//  KevinDemo
+//
+//  Created by Edgar Žigis on 2021-12-11.
+//
+
+import Foundation
+import SwiftUI
+import Kevin
+
+public class DemoKevinTheme : KevinTheme {
+    
+    public override init() {
+        super.init()
+        self.primaryTextColor = UIColor(Color("PrimaryTextColor", bundle: nil))
+        self.secondaryBackgroundColor = UIColor(Color("SecondaryBackgroundColor", bundle: nil))
+        self.primaryBackgroundColor = UIColor(Color("PrimaryBackgroundColor", bundle: nil))
+        self.selectedOnPrimaryColor = UIColor(Color("SelectedOnPrimaryColor", bundle: nil))
+        //  for more properties check KevinTheme class
+    }
+}
