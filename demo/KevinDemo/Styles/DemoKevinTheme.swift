f963f7f1008a3caf894ebd5061934a17eb108cab
Edgar Žigis <tranced.freak@gmail.com>
2022-02-02 13:13:02 +0200

    MOBILE-597: Update demo app

--- demo/KevinDemo/Styles/DemoKevinTheme.swift
+++ demo/KevinDemo/Styles/DemoKevinTheme.swift
@@ -15,6 +15,8 @@ public class DemoKevinTheme : KevinTheme {
         super.init()
         self.primaryTextColor = UIColor(Color("PrimaryTextColor", bundle: nil))
         self.primaryBackgroundColor = UIColor(Color("PrimaryBackgroundColor", bundle: nil))
+        self.secondaryBackgroundColor = UIColor(Color("SecondaryBackgroundColor", bundle: nil))
+        self.selectedOnSecondaryColor = UIColor(Color("SelectedOnSecondaryColor", bundle: nil))
         //  for more properties check KevinTheme class
     }
 }
