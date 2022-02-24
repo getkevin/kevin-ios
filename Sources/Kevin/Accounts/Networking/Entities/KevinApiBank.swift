7b731754a04051d02333c92b8372c2d688242e06
Daniels Klinge <daniels.klinge@kevin.eu>
2022-02-24 13:00:06 +0200

    MOBILE-674: Load dark mode logos if dark theme if active

--- Sources/Kevin/Accounts/Networking/Entities/KevinApiBank.swift
+++ Sources/Kevin/Accounts/Networking/Entities/KevinApiBank.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 public class ApiBank {
     
@@ -15,7 +16,25 @@ public class ApiBank {
     public let officialName: String?
     public let countryCode: String
     public let isSandbox: Bool
-    public let imageUri: String
+    private var _imageUri: String = ""
+    public var imageUri: String {
+        get {
+            var imageUri = _imageUri
+            
+            if !UIApplication.shared.isLightThemedInterface {
+                let imageUriParts = _imageUri.components(separatedBy: "images/")
+                
+                if imageUriParts.count > 1 {
+                    imageUri = "\(imageUriParts.first!)images/white/\(imageUriParts.last!)"
+                }
+            }
+            
+            return imageUri
+        }
+        set {
+            _imageUri = newValue
+        }
+    }
     public let bic: String?
     public let isBeta: Bool
     
@@ -34,7 +53,7 @@ public class ApiBank {
         self.officialName = officialName
         self.countryCode = countryCode
         self.isSandbox = isSandbox
-        self.imageUri = imageUri
+        self._imageUri = imageUri
         self.bic = bic
         self.isBeta = isBeta
     }
