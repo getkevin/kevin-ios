19c76f663e849925adc1c9ea3a91b028059208e9
Daniels Klinge <daniels.klinge@kevin.eu>
2023-09-05 12:40:12 +0300

    Post comment refactoring

--- Sources/Kevin/Core/Styles/KevinTheme.swift
+++ Sources/Kevin/Core/Styles/KevinTheme.swift
@@ -21,10 +21,10 @@ open class KevinTheme {
     }
     
     public struct GeneralStyle {
-        public var primaryBackgroundColor = UIColor(named: "BackgroundPrimary", in: Bundle.current, compatibleWith: nil)!
-        public var primaryTextColor = UIColor(named: "TextPrimary", in: Bundle.current, compatibleWith: nil)!
-        public var secondaryTextColor = UIColor(named: "TextSecondary", in: Bundle.current, compatibleWith: nil)!
-        public var actionTextColor = UIColor(named: "CTABackgroundPrimary", in: Bundle.current, compatibleWith: nil)!
+        public var primaryBackgroundColor = UIColor(named: "BackgroundPrimary", in: .current, compatibleWith: nil)!
+        public var primaryTextColor = UIColor(named: "TextPrimary", in: .current, compatibleWith: nil)!
+        public var secondaryTextColor = UIColor(named: "TextSecondary", in: .current, compatibleWith: nil)!
+        public var actionTextColor = UIColor(named: "CTABackgroundPrimary", in: .current, compatibleWith: nil)!
         public var primaryFont = UIFont.systemFont(ofSize: 23, weight: .bold)
         public var secondaryFont = UIFont.systemFont(ofSize: 16, weight: .regular)
 
@@ -32,19 +32,19 @@ open class KevinTheme {
     }
     
     public struct NavigationBarStyle {
-        public var titleColor = UIColor(named: "TextPrimary", in: Bundle.current, compatibleWith: nil)!
-        public var tintColor = UIColor(named: "KevinRed", in: Bundle.current, compatibleWith: nil)!
-        public var backgroundColorLightMode = UIColor(named: "BackgroundPrimary", in: Bundle.current, compatibleWith: nil)!
-        public var backgroundColorDarkMode = UIColor(named: "BackgroundPrimary", in: Bundle.current, compatibleWith: nil)!
-        public var backButtonImage = UIImage(named: "backButtonIcon", in: Bundle.current, compatibleWith: nil)
-        public var closeButtonImage = UIImage(named: "closeButtonIcon", in: Bundle.current, compatibleWith: nil)
+        public var titleColor = UIColor(named: "TextPrimary", in: .current, compatibleWith: nil)!
+        public var tintColor = UIColor(named: "KevinRed", in: .current, compatibleWith: nil)!
+        public var backgroundColorLightMode = UIColor(named: "BackgroundPrimary", in: .current, compatibleWith: nil)!
+        public var backgroundColorDarkMode = UIColor(named: "BackgroundPrimary", in: .current, compatibleWith: nil)!
+        public var backButtonImage = UIImage(named: "backButtonIcon", in: .current, compatibleWith: nil)
+        public var closeButtonImage = UIImage(named: "closeButtonIcon", in: .current, compatibleWith: nil)
         
         public init() { }
     }
     
     public struct SheetPresentationStyle {
-        public var dragIndicatorTintColor = UIColor(named: "TextTertiary", in: Bundle.current, compatibleWith: nil)!
-        public var backgroundColor = UIColor(named: "BackgroundPrimary", in: Bundle.current, compatibleWith: nil)!
+        public var dragIndicatorTintColor = UIColor(named: "TextTertiary", in: .current, compatibleWith: nil)!
+        public var backgroundColor = UIColor(named: "BackgroundPrimary", in: .current, compatibleWith: nil)!
         public var titleLabelFont = UIFont.systemFont(ofSize: 18, weight: .medium)
         public var cornerRadius: CGFloat = 32
         
@@ -58,14 +58,14 @@ open class KevinTheme {
     }
     
     public struct GridTableStyle {
-        public var cellBackgroundColor = UIColor(named: "ForegroundPrimary", in: Bundle.current, compatibleWith: nil)!
+        public var cellBackgroundColor = UIColor(named: "ForegroundPrimary", in: .current, compatibleWith: nil)!
         public var cellCornerRadius: CGFloat = 8
         public var cellBorderWidth: CGFloat = 0
         public var cellBorderColor = UIColor.clear
         
-        public var cellSelectedBackgroundColor = UIColor(named: "ForegroundPrimary", in: Bundle.current, compatibleWith: nil)!
+        public var cellSelectedBackgroundColor = UIColor(named: "ForegroundPrimary", in: .current, compatibleWith: nil)!
         public var cellSelectedBorderWidth: CGFloat = 2
-        public var cellSelectedBorderColor = UIColor(named: "IconBlueToYellow", in: Bundle.current, compatibleWith: nil)!
+        public var cellSelectedBorderColor = UIColor(named: "IconBlueToYellow", in: .current, compatibleWith: nil)!
         
         public init() { }
     }
@@ -73,23 +73,23 @@ open class KevinTheme {
     public struct ListTableStyle {
         public var cornerRadius: CGFloat = 11
         public var isOccupyingFullWidth = false
-        public var cellBackgroundColor = UIColor(named: "ForegroundPrimary", in: Bundle.current, compatibleWith: nil)!
-        public var cellSelectedBackgroundColor = UIColor(named: "BackgroundSelectedPrimary", in: Bundle.current, compatibleWith: nil)!
+        public var cellBackgroundColor = UIColor(named: "ForegroundPrimary", in: .current, compatibleWith: nil)!
+        public var cellSelectedBackgroundColor = UIColor(named: "BackgroundSelectedPrimary", in: .current, compatibleWith: nil)!
         
         public var titleLabelFont = UIFont.systemFont(ofSize: 16, weight: .medium)
-        public var chevronImage = UIImage(named: "chevronIcon", in: Bundle.current, compatibleWith: nil)
+        public var chevronImage = UIImage(named: "chevronIcon", in: .current, compatibleWith: nil)
         
         public init() { }
     }
     
     public struct NavigationLinkStyle {
-        public var backgroundColor = UIColor(named: "ForegroundPrimary", in: Bundle.current, compatibleWith: nil)!
+        public var backgroundColor = UIColor(named: "ForegroundPrimary", in: .current, compatibleWith: nil)!
         public var titleLabelFont = UIFont.systemFont(ofSize: 16, weight: .medium)
-        public var selectedBackgroundColor = UIColor(named: "ForegroundPrimary", in: Bundle.current, compatibleWith: nil)!
+        public var selectedBackgroundColor = UIColor(named: "ForegroundPrimary", in: .current, compatibleWith: nil)!
         public var cornerRadius: CGFloat = 11
         public var borderWidth: CGFloat = 0
-        public var borderColor = UIColor(named: "CTABackgroundPrimary", in: Bundle.current, compatibleWith: nil)!
-        public var chevronImage = UIImage(named: "chevronIcon", in: Bundle.current, compatibleWith: nil)
+        public var borderColor = UIColor(named: "CTABackgroundPrimary", in: .current, compatibleWith: nil)!
+        public var chevronImage = UIImage(named: "chevronIcon", in: .current, compatibleWith: nil)
         
         public init() { }
     }
@@ -97,8 +97,8 @@ open class KevinTheme {
     public struct MainButtonStyle {
         public var width = UIScreen.main.bounds.width - 32
         public var height: CGFloat = 48
-        public var backgroundColor = UIColor(named: "CTABackgroundPrimary", in: Bundle.current, compatibleWith: nil)!
-        public var titleLabelTextColor = UIColor(named: "CTATextPrimary", in: Bundle.current, compatibleWith: nil)!
+        public var backgroundColor = UIColor(named: "CTABackgroundPrimary", in: .current, compatibleWith: nil)!
+        public var titleLabelTextColor = UIColor(named: "CTATextPrimary", in: .current, compatibleWith: nil)!
         public var titleLabelFont = UIFont.systemFont(ofSize: 16, weight: .medium)
         public var cornerRadius: CGFloat = 8
         public var shadowRadius: CGFloat = 0
@@ -110,12 +110,12 @@ open class KevinTheme {
     }
     
     public struct EmptyStateStyle {
-        public var titleTextColor = UIColor(named: "TextPrimary", in: Bundle.current, compatibleWith: nil)!
+        public var titleTextColor = UIColor(named: "TextPrimary", in: .current, compatibleWith: nil)!
         public var titleFont = UIFont.systemFont(ofSize: 22, weight: .semibold)
-        public var subtitleTextColor =  UIColor(named: "TextSecondary", in: Bundle.current, compatibleWith: nil)!
+        public var subtitleTextColor =  UIColor(named: "TextSecondary", in: .current, compatibleWith: nil)!
         public var subtitleFont = UIFont.systemFont(ofSize: 16, weight: .regular)
         public var cornerRadius: CGFloat = 11
-        public var iconTintColor = UIColor(named: "KevinRed", in: Bundle.current, compatibleWith: nil)!
+        public var iconTintColor = UIColor(named: "KevinRed", in: .current, compatibleWith: nil)!
         
         public init() { }
     }
